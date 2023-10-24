c148a95f70b10b82119add5b5879d203fd1ad5e8
mochi-neko <t.o.e.4315@gmail.com>
2023-10-24 12:13:08 +0900

    Implement decode_box

--- src/blaze_face/blaze_face.rs
+++ src/blaze_face/blaze_face.rs
@@ -1,8 +1,11 @@
 // Reference implementation:
 // https://github.com/hollance/BlazeFace-PyTorch/blob/master/blazeface.py
 
-use candle_core::{Result, Tensor};
+use std::ops::Add;
+
+use candle_core::{IndexOp, Result, Tensor};
 use candle_nn::VarBuilder;
+use half::f16;
 
 use super::{
     blaze_face_back_model::BlazeFaceBackModel,
@@ -36,7 +39,7 @@ impl BlazeFace {
         score_clipping_thresh: f32,
         min_score_thresh: f32,
         min_suppression_threshold: f32,
-    ) -> Result<BlazeFace> {
+    ) -> Result<Self> {
         match model_type {
             | ModelType::Back => {
                 let model = BlazeFaceBackModel::load(variables)?;
@@ -65,26 +68,120 @@ impl BlazeFace {
         }
     }
 
-    pub(crate) fn forward(
+    fn forward(
         &self,
-        xs: &Tensor,
-    ) -> Result<(Tensor, Tensor)> {
+        xs: &Tensor, // back:(batch_size, 3, 256, 256) or front:(batch_size, 3, 128, 128)
+    ) -> Result<(Tensor, Tensor)> // (batch_size, 896, 16), (batch_size, 896, 1)
+    {
         self.model.forward(xs)
     }
 
     fn convert_tensors_to_detections(
         &self,
-        raw_boxes: &Tensor,
-        raw_scores: &Tensor,
+        raw_boxes: &Tensor,  // (batch_size, 896, 16)
+        raw_scores: &Tensor, // (batch_size, 896, 1)
     ) -> Result<Vec<Tensor>> {
         unimplemented!()
     }
 
     fn decode_boxes(
-        &self,
-        raw_boxes: &Tensor,
-    ) -> Result<Tensor> {
-        unimplemented!()
+        raw_boxes: &Tensor, // (batch_size, 896, 16)
+        anchors: &Tensor,   // (896, 4)
+        config: &BlazeFaceConfig,
+    ) -> Result<Tensor> // (batch_size, 896, 16)
+    {
+        let boxes = Tensor::zeros_like(raw_boxes)?; // (batch_size, 896, 16)
+
+        let x_scale =
+            Tensor::from_slice(&[config.x_scale], 1, raw_boxes.device())?; // (1)
+        let y_scale =
+            Tensor::from_slice(&[config.y_scale], 1, raw_boxes.device())?; // (1)
+        let w_scale =
+            Tensor::from_slice(&[config.w_scale], 1, raw_boxes.device())?; // (1)
+        let h_scale =
+            Tensor::from_slice(&[config.h_scale], 1, raw_boxes.device())?; // (1)
+
+        let two = Tensor::from_slice(
+            &[f16::from_f32(2.)],
+            1,
+            raw_boxes.device(),
+        )?; // (1)
+
+        let x_anchor = anchors.i((.., 0))?; // (896)
+        let y_anchor = anchors.i((.., 1))?; // (896)
+        let w_anchor = anchors.i((.., 2))?; // (896)
+        let h_anchor = anchors.i((.., 3))?; // (896)
+
+        let x_center = raw_boxes
+            .i((.., .., 0))? // (batch_size, 896)
+            .broadcast_div(&x_scale)? // / (1)
+            .broadcast_mul(&w_anchor)? // * (896)
+            .broadcast_add(&x_anchor)?; // + (896)
+                                        // = (batch_size, 896)
+
+        let y_center = raw_boxes
+            .i((.., .., 1))?
+            .broadcast_div(&y_scale)?
+            .broadcast_mul(&h_anchor)?
+            .broadcast_add(&y_anchor)?;
+
+        let w = raw_boxes
+            .i((.., .., 2))? // (batch_size, 896)
+            .broadcast_div(&w_scale)? // / (1)
+            .broadcast_mul(&w_anchor)?; // * (896)
+                                        // = (batch_size, 896)
+
+        let h = raw_boxes
+            .i((.., .., 3))?
+            .broadcast_div(&h_scale)?
+            .broadcast_mul(&h_anchor)?;
+
+        let x_min = (&x_center - w.broadcast_div(&two)?)?; // (batch_size, 896)
+        let x_max = (&x_center + w.broadcast_div(&two)?)?;
+        let y_min = (&y_center - h.broadcast_div(&two)?)?;
+        let y_max = (&y_center + h.broadcast_div(&two)?)?;
+
+        // Bounding box
+        boxes
+            .i((.., .., 0))? // (batch_size, 896)
+            .add(y_min)?; // + (batch_size, 896)
+        boxes
+            .i((.., .., 1))?
+            .add(x_min)?;
+        boxes
+            .i((.., .., 2))?
+            .add(y_max)?;
+        boxes
+            .i((.., .., 3))?
+            .add(x_max)?;
+
+        // Face keypoints: right_eye, left_eye, nose, mouth, right_ear, left_ear
+        for k in 0..6 {
+            let offset = 4 + k * 2;
+
+            let keypoint_x = raw_boxes
+                .i((.., .., offset))? // (batch_size, 896)
+                .broadcast_div(&x_scale)? // / (1)
+                .broadcast_mul(&w_anchor)? // * (896)
+                .broadcast_add(&x_anchor)?; // + (896)
+                                            // = (batch_size, 896)
+
+            let keypoint_y = raw_boxes
+                .i((.., .., offset + 1))?
+                .broadcast_div(&y_scale)?
+                .broadcast_mul(&h_anchor)?
+                .broadcast_add(&y_anchor)?;
+
+            // Keypoint
+            boxes
+                .i((.., .., offset))
+                .add(keypoint_x)?;
+            boxes
+                .i((.., .., offset + 1))
+                .add(keypoint_y)?;
+        }
+
+        Ok(boxes) // (batch_size, 896, 16)
     }
 
     fn process_scores(
@@ -197,5 +294,29 @@ mod tests {
 
         assert_eq!(output.0.dims(), &[batch_size, 896, 1,]);
         assert_eq!(output.1.dims(), &[batch_size, 896, 16,]);
+    }
+
+    #[test]
+    fn test_decode_boxes() {
+        // Set up the device and dtype
+        let device = Device::Cpu;
+        let dtype = DType::F16;
+        let batch_size = 1;
+
+        // Set up the anchors and configuration
+        let anchors = Tensor::read_npy("src/blaze_face/data/anchorsback.npy")
+            .unwrap()
+            .to_dtype(dtype)
+            .unwrap();
+        let config = BlazeFaceConfig::back(100., 0.65, 0.3);
+
+        // Set up the input Tensor
+        let input =
+            Tensor::zeros((batch_size, 896, 16), dtype, &device).unwrap();
+
+        // Decode boxes
+        let boxes = BlazeFace::decode_boxes(&input, &anchors, &config).unwrap();
+
+        assert_eq!(boxes.dims(), &[batch_size, 896, 16]);
     }
 }
--- src/blaze_face/blaze_face_config.rs
+++ src/blaze_face/blaze_face_config.rs
@@ -1,17 +1,19 @@
 // Reference implementation:
 // https://github.com/hollance/BlazeFace-PyTorch/blob/master/blazeface.py
 
+use half::f16;
+
 pub struct BlazeFaceConfig {
     pub(crate) num_classes: usize,
     pub(crate) num_anchors: usize,
     pub(crate) num_coords: usize,
-    pub(crate) x_scale: f32,
-    pub(crate) y_scale: f32,
-    pub(crate) h_scale: f32,
-    pub(crate) w_scale: f32,
-    pub(crate) score_clipping_thresh: f32,
-    pub(crate) min_score_thresh: f32,
-    pub(crate) min_suppression_threshold: f32,
+    pub(crate) x_scale: f16,
+    pub(crate) y_scale: f16,
+    pub(crate) h_scale: f16,
+    pub(crate) w_scale: f16,
+    pub(crate) score_clipping_thresh: f16,
+    pub(crate) min_score_thresh: f16,
+    pub(crate) min_suppression_threshold: f16,
 }
 
 impl BlazeFaceConfig {
@@ -24,13 +26,13 @@ impl BlazeFaceConfig {
             num_classes: 1,
             num_anchors: 896,
             num_coords: 16,
-            x_scale: 128.,
-            y_scale: 128.,
-            h_scale: 128.,
-            w_scale: 128.,
-            score_clipping_thresh,
-            min_score_thresh,
-            min_suppression_threshold,
+            x_scale: f16::from_f32(256.),
+            y_scale: f16::from_f32(256.),
+            h_scale: f16::from_f32(256.),
+            w_scale: f16::from_f32(256.),
+            score_clipping_thresh: f16::from_f32(score_clipping_thresh),
+            min_score_thresh: f16::from_f32(min_score_thresh),
+            min_suppression_threshold: f16::from_f32(min_suppression_threshold),
         }
     }
 
@@ -43,13 +45,13 @@ impl BlazeFaceConfig {
             num_classes: 1,
             num_anchors: 896,
             num_coords: 16,
-            x_scale: 128.,
-            y_scale: 128.,
-            h_scale: 128.,
-            w_scale: 128.,
-            score_clipping_thresh,
-            min_score_thresh,
-            min_suppression_threshold,
+            x_scale: f16::from_f32(128.),
+            y_scale: f16::from_f32(128.),
+            h_scale: f16::from_f32(128.),
+            w_scale: f16::from_f32(128.),
+            score_clipping_thresh: f16::from_f32(score_clipping_thresh),
+            min_score_thresh: f16::from_f32(min_score_thresh),
+            min_suppression_threshold: f16::from_f32(min_suppression_threshold),
         }
     }
 }
